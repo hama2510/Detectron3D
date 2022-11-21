6f39881e43e8bea4c4d62c183c34c3e5b98879e1
Nguyen Trung Kien 2 <kien.nt2510@gmail.com>
2022-11-21 21:54:26 +0900

    edit loss function

--- src/criterion/losses.py
+++ src/criterion/losses.py
@@ -13,14 +13,29 @@ class Criterion(nn.Module):
         
     def focal_loss(self, pred, target, esp=1e-8):
         pred, target = self.flattern(pred, target)
-        pred = torch.clamp(torch.nn.functional.softmax(pred,dim=2), min=1e-4, max=1-1e-4)
+        # pred = torch.clamp(torch.nn.functional.softmax(pred,dim=2), min=1e-4, max=1-1e-4)
+        # pred = torch.clamp(pred, min=1e-4, max=1-1e-4)
+        pos_inds = target.eq(1)
+        neg_inds = target.lt(1)
 
-        pos_index = torch.eq(target, 1)
-        pt = 1 - pred
-        pt[pos_index] = pred[pos_index]
-        pt = torch.clamp(pt, esp, 1-esp)
-        loss = -1*self.alpha*(torch.pow((1-pt), self.gamma))*torch.log(pt)
-        loss = loss.mean()
+        # neg_weights = torch.pow(1 - target[neg_inds], 4)
+
+        loss = 0
+        pos_pred = pred[pos_inds]
+        neg_pred = pred[neg_inds]
+
+        pos_loss = torch.log(pos_pred) * torch.pow(1 - pos_pred, self.gamma)
+        # neg_loss = torch.log(1 - neg_pred) * torch.pow(neg_pred, self.gamma) * neg_weights
+        neg_loss = torch.log(1 - neg_pred) * torch.pow(neg_pred, self.gamma)
+
+        num_pos  = pos_inds.float().sum()
+        pos_loss = pos_loss.sum()
+        neg_loss = neg_loss.sum()
+
+        if pos_pred.nelement() == 0:
+            loss = loss - neg_loss
+        else:
+            loss = loss - (pos_loss + neg_loss) / num_pos
         return loss
 
     def to_device(self, item):
@@ -30,9 +45,9 @@ class Criterion(nn.Module):
 
     def flattern(self, pred, target):
         shape = pred.shape
-        pred = torch.reshape(pred, (shape[0], shape[1], shape[2]*shape[3])).view(shape[0], shape[2]*shape[3], shape[1])
+        pred = torch.reshape(pred.view(shape[0], shape[2], shape[3], shape[1]), (shape[0]*shape[1]*shape[2], shape[3]))
         shape = target.shape
-        target = torch.reshape(target, (shape[0], shape[1]*shape[2], shape[3]))
+        target = torch.reshape(target, (shape[0]*shape[1]*shape[2], shape[3]))
         return pred, target
 
     def smooth_l1_loss(self, pred, target):
@@ -41,12 +56,29 @@ class Criterion(nn.Module):
 
     def bce_loss(self, pred, target):
         pred, target = self.flattern(pred, target)
-        return nn.BCELoss()(pred, target)
+        loss = nn.BCELoss()(pred, target)
+        # pos_inds = target.eq(1)
+        # loss = nn.BCELoss(reduction='none')(pred, target)*pos_index.float().sum(axis=1)
+        # loss = loss.sum()/pos_inds.float().sum()
+        return loss
+
+    def kl_loss(self, pred, target):
+        pred, target = self.flattern(pred, target)
+        pred = torch.clamp(pred, min=1e-4, max=1-1e-4)
+        kl_loss = target*torch.log(target/pred)
+        # pos_inds = target.eq(1)
+        # loss = nn.BCELoss(reduction='none')(pred, target).mean(axis=1)*pos_index.float().sum(axis=1)
+        # loss = loss.sum()/pos_inds.float().sum()
+        return loss
 
     def cross_entropy_loss(self, pred, target):
         pred, target = self.flattern(pred, target)
-        pred = torch.clamp(torch.nn.functional.softmax(pred,dim=2), min=1e-4, max=1-1e-4)
-        return nn.CrossEntropyLoss()(pred, target)
+        loss = nn.CrossEntropyLoss()(pred, target)
+        # pred = torch.clamp(torch.nn.functional.softmax(pred,dim=2), min=1e-4, max=1-1e-4)
+        # pos_inds = target.eq(1)
+        # loss = nn.CrossEntropyLoss(reduction='none')(pred, target).mean(axis=1)*pos_inds.float().sum(axis=1)
+        # loss = loss.sum()/.float().sum()
+        return loss
 
     def stride_to_feat_level(self, stride):
         return int(np.log2(stride))
@@ -59,6 +91,7 @@ class Criterion(nn.Module):
         category_loss = self.focal_loss(pred['category'], target['category'])
         attribute_loss = self.cross_entropy_loss(pred['attribute'], target['attribute'])
         centerness_loss = self.bce_loss(pred['centerness'], target['centerness'])
+        # centerness_loss = self.kl_loss(pred['centerness'], target['centerness'])
         offset_loss = self.smooth_l1_loss(pred['offset'], target['offset'])
         depth_loss = self.smooth_l1_loss(torch.exp(pred['depth']), target['depth'])
         size_loss = self.smooth_l1_loss(pred['size'], target['size'])

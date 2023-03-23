fix rotation mapback and CE loss

src/criterion/losses.py
@@ -106,7 +106,7 @@ class Criterion(nn.Module):
             if num_pos==0:
                 return 0
             else:
-                return (nn.CrossEntropyLoss(reduction='none')(pred, target)*masked).sum()/num_pos
+                return (nn.CrossEntropyLoss(reduction='none')(pred, target)*masked).mean(dim=1).sum()/num_pos
 
     def stride_to_feat_level(self, stride):
         return int(np.log2(stride))

src/model/fcos3d_detector.py
@@ -167,12 +167,7 @@ class FCOSTransformer():
                 rotation = np.arcsin(rotation_map[idx[0],idx[1],0])
                 dir = np.argmax(dir_map[idx[0],idx[1],:])
                 if dir==0:
-                    if rotation<0:
-                        rotation-=np.pi/2
-                    else:
-                        rotation+=np.pi/2
-                else:
-                    if rotation<0:
+                    if rotation>0:
                         rotation+=np.pi/2
                     else:
                         rotation-=np.pi/2
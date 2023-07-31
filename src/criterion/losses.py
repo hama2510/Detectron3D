fix rotation and loss

src/criterion/losses.py
@@ -98,17 +98,40 @@ class Criterion(nn.Module):
 #         # loss = loss.sum()/pos_inds.float().sum()
 #         return loss
 
+#     def cross_entropy_loss(self, pred, target, masked):
+#         pred, target = self.flattern(pred, target)
+# #         pred = torch.clamp(pred, min=self.esp, max=1-self.esp)
+#         if masked is None:
+#             return nn.CrossEntropyLoss()(pred, target)
+#         else:
+#             num_pos = masked.sum()
+#             if num_pos==0:
+#                 return 0
+#             else:
+#                 return (nn.CrossEntropyLoss(reduction='none')(pred, target)*masked).sum()/num_pos
+            
     def cross_entropy_loss(self, pred, target, masked):
         pred, target = self.flattern(pred, target)
-#         pred = torch.clamp(pred, min=self.esp, max=1-self.esp)
-        if masked is None:
-            return nn.CrossEntropyLoss()(pred, target)
+        pred = torch.clamp(pred, min=self.esp, max=1-self.esp)
+        
+        pos_inds = target.eq(1)
+        neg_inds = target.lt(1)
+        
+        pos_pred = pred[pos_inds]
+        neg_pred = pred[neg_inds]
+
+        pos_loss = torch.log(pos_pred)
+        neg_loss = torch.log(1 - neg_pred)
+
+        num_pos  = pos_inds.float().sum()
+        pos_loss = pos_loss.sum()
+        neg_loss = neg_loss.sum()
+
+        if num_pos == 0:
+            loss = 0 - neg_loss
         else:
-            num_pos = masked.sum()
-            if num_pos==0:
-                return 0
-            else:
-                return (nn.CrossEntropyLoss(reduction='none')(pred, target)*masked).sum()/num_pos
+            loss = 0 - (pos_loss + neg_loss) / num_pos
+        return loss
 
     def stride_to_feat_level(self, stride):
         return int(np.log2(stride))
@@ -125,7 +148,7 @@ class Criterion(nn.Module):
 
         category_loss = self.focal_loss(pred['category'], target['category'])
         attribute_loss = self.cross_entropy_loss(pred['attribute'], target['attribute'], masked)
-        centerness_loss = self.bce_loss(pred['centerness'], target['centerness'], masked)
+        centerness_loss = self.cross_entropy_loss(pred['centerness'], target['centerness'], masked)
         offset_loss = self.smooth_l1_loss(pred['offset'], target['offset'], masked)
         depth_loss = self.smooth_l1_loss(torch.exp(pred['depth']), target['depth'], masked)
         size_loss = self.smooth_l1_loss(pred['size'], target['size'], masked)

src/data/nuscene_dataset.py
@@ -291,11 +291,11 @@ class NusceneDataset(Dataset):
             #         )
             #         if self.rotation_encode == "sin_pi_and_bin":
             #             rad, dir_cls = self.rotation_angle_to_sin_pi_and_bin(
-            #                 ann["rotation_angle_rad"]
+            #                 ann["yaw_angle_rad"]
             #             )
             #         elif self.rotation_encode == "pi_and_minus_pi":
             #             rad = self.rotation_angle_to_pi_and_minus_pi(
-            #                 ann["rotation_angle_rad"]
+            #                 ann["yaw_angle_rad"]
             #             )
             #             dir_cls = 0
 
@@ -358,11 +358,11 @@ class NusceneDataset(Dataset):
                         box_2d = np.asarray(box["box_2d"], dtype=object) // stride
                         if self.rotation_encode == "sin_pi_and_bin":
                             rad, dir_cls = self.rotation_angle_to_sin_pi_and_bin(
-                                ann["rotation_angle_rad"]
+                                ann["yaw_angle_rad"]
                             )
                         elif self.rotation_encode == "pi_and_minus_pi":
                             rad = self.rotation_angle_to_pi_and_minus_pi(
-                                ann["rotation_angle_rad"]
+                                ann["yaw_angle_rad"]
                             )
                             dir_cls = 0
 

src/data/nuscene_dataset_fcos3d.py
@@ -201,9 +201,9 @@ class NusceneDatasetFCOS3D(Dataset):
                     box_2d = (
                         np.asarray(ann["box_2d"], dtype=object) // stride * self.resize
                     )
-                    #                     rad, dir_cls = self.rotation_angle_to_pi_and_bin(ann['rotation_angle_rad'])
+                    #                     rad, dir_cls = self.rotation_angle_to_pi_and_bin(ann['yaw_angle_rad'])
                     rad, dir_cls = self.rotation_angle_to_sin_pi_and_bin(
-                        ann["rotation_angle_rad"]
+                        ann["yaw_angle_rad"]
                     )
 
                     category_onehot = self.gen_category_onehot(ann["category"])
@@ -260,9 +260,9 @@ class NusceneDatasetFCOS3D(Dataset):
                         )
                         box = boxes[0]
                         box_2d = np.asarray(box["box_2d"], dtype=object) // stride
-                        #                     rad, dir_cls = self.rotation_angle_to_pi_and_bin(box['rotation_angle_rad'])
+                        #                     rad, dir_cls = self.rotation_angle_to_pi_and_bin(box['yaw_angle_rad'])
                         rad, dir_cls = self.rotation_angle_to_sin_pi_and_bin(
-                            box["rotation_angle_rad"]
+                            box["yaw_angle_rad"]
                         )
 
                         category_onehot = self.gen_category_onehot(box["category"])

src/model/fcos3d.py
@@ -52,7 +52,7 @@ class ClassificationHead(nn.Module):
         x = self.convs(x)
         outs = OrderedDict()
         outs['category'] = self.sigmoid(self.conv_cate(x))
-        outs['attribute'] = self.conv_attr(x)
+        outs['attribute'] = self.sigmoid(self.conv_attr(x))
         return outs
 
 class RegressionHead(nn.Module):
@@ -78,6 +78,6 @@ class RegressionHead(nn.Module):
         outs['depth'] = self.relu(self.conv_depth(x))
         outs['size'] = self.relu(self.conv_size(x))
         outs['rotation'] = self.tanh(self.conv_rotation(x))
-        outs['dir'] = self.conv_dir(x)
+        outs['dir'] = self.sigmoid(self.conv_dir(x))
         outs['velocity'] = self.conv_velo(x)
         return outs

src/utils/camera.py
@@ -80,20 +80,27 @@ def coord_2d_to_3d(coord_2d, depth, calibration_matrix, calibrated=True):
     return coord_3d
 
 
-# def rotate_box(box, quaternion):
-#     q = Quaternion(axis=[0,0,1], angle=quaternion_yaw(quaternion))
-#     box.center = np.dot(quaternion.rotation_matrix, box.center)
-#     box.orientation = q * box.orientation
-#     box.velocity = np.dot(quaternion.rotation_matrix, box.velocity)
-#     return box 
+def rotate_box_by_yaw(box, quaternion):
+    yaw = Quaternion(axis=[0,0,1], angle=quaternion.yaw_pitch_roll[0])
+    box.center = np.dot(quaternion.rotation_matrix, box.center)
+    box.orientation = yaw * box.orientation
+    box.velocity = np.dot(quaternion.rotation_matrix, box.velocity)
 
-def sensor_coord_to_real_coord(coord, size, rotation, calibration_matrix):
+    return box 
+
+def sensor_coord_to_real_coord(coord, size, rotation, calibration_matrix, rotate_yaw_only=False):
     sensor_R_quaternion, sensor_t, ego_R_quaternion, ego_t = calibration_matrix['sensor_R_quaternion'], calibration_matrix['sensor_t'], calibration_matrix['ego_R_quaternion'], calibration_matrix['ego_t']
     box = Box(center=coord, size=size, orientation=rotation)
-    box.rotate(Quaternion(sensor_R_quaternion))
-    box.translate(np.array(sensor_t))
-    box.rotate(Quaternion(ego_R_quaternion))
-    box.translate(np.array(ego_t))
+    if not rotate_yaw_only:
+        box.rotate(Quaternion(sensor_R_quaternion))
+        box.translate(np.array(sensor_t))
+        box.rotate(Quaternion(ego_R_quaternion))
+        box.translate(np.array(ego_t))
+    else:
+        box = rotate_box_by_yaw(box, Quaternion(sensor_R_quaternion))
+        box.translate(np.array(sensor_t))
+        box = rotate_box_by_yaw(box, Quaternion(ego_R_quaternion))
+        box.translate(np.array(ego_t))
     return box
 
 # def sensor_coord_to_real_coord_y_axis(coord, size, rotation, calibration_matrix):